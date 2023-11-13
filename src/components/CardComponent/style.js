f73bef5379a00438bd00e783e8dbbfd71d45c368
Ngo Thang <nqthang21@gmail.com>
2023-11-13 08:55:39 +0700

    add paypal, fix bug

--- src/components/CardComponent/style.js
+++ src/components/CardComponent/style.js
@@ -1,5 +1,5 @@
-import { Card } from "antd";
-import styled from "styled-components";
+import { Card } from 'antd';
+import styled from 'styled-components';
 
 export const WrapperCardStyle = styled(Card)`
     width: 200px;
@@ -8,17 +8,17 @@ export const WrapperCardStyle = styled(Card)`
         width: 200px;
     },
     position: relative;
-    background-color: ${props => props.disabled ? '#ccc' : '#fff'};
-    cursor: ${props => props.disabled ? 'not-allowed' : 'pointer'}
-`
+    background-color: ${(props) => (props.disabled ? '#ccc' : '#fff')};
+    cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')}
+`;
 
 export const StyleNameProduct = styled.div`
-    font-weight: 400;
-    font-size: 12px;
+    font-weight: 600;
+    font-size: 14px;
     line-height: 16px;
     color: rgb(56, 56, 61);
     font-weight: 400;
-`
+`;
 
 export const WrapperReportText = styled.div`
     font-size: 11px;
@@ -26,22 +26,22 @@ export const WrapperReportText = styled.div`
     display: flex;
     align-items: center;
     margin: 6px 0 0px;
-`
+`;
 
 export const WrapperPriceText = styled.div`
     color: rgb(255, 66, 78);
     font-size: 16px;
     font-weight: 500;
-`
+`;
 
 export const WrapperDiscountText = styled.span`
     color: rgb(255, 66, 78);
     font-size: 12px;
     font-weight: 500;
-`
+`;
 
 export const WrapperStyleTextSell = styled.span`
     font-size: 15px;
     line-height: 24px;
-    color: rgb(120, 120, 120)
-`
+    color: rgb(120, 120, 120);
+`;
